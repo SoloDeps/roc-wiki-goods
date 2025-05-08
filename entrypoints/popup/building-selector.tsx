fb052e35c438b157fcc4ca7f844309a7f8d6170e
SoloDeps <solodevops31@gmail.com>
2025-05-08 13:15:30 +0200

    fix: add style on reset button & changelog

--- entrypoints/popup/building-selector.tsx
+++ entrypoints/popup/building-selector.tsx
@@ -93,7 +93,7 @@ export default function BuildingSelector({
               <path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8" />
               <path d="M21 3v5h-5" />
             </svg>
-            <span>Reset</span>
+            <span className="block text-xs">Reset</span>
           </button>
         )}
       </div>
